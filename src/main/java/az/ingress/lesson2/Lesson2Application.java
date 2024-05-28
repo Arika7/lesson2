8521af291fc6d286f5837833be6b9fa4d97b03f6
Arika_notebook <arif.quliev39@gmail.com>
2024-05-29 01:39:10 +0400

    added env variables

--- src/main/java/az/ingress/lesson2/Lesson2Application.java
+++ src/main/java/az/ingress/lesson2/Lesson2Application.java
@@ -1,5 +1,6 @@
 package az.ingress.lesson2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class Lesson2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Lesson2Application.class, args);
 	}
-
 }
